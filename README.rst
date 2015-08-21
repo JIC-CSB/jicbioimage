27bad1175ccd2462932a9bd9743cc8f64684c66c
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-08-21 15:18:07 +0100

    Initial file import.

---
+++ README.rst
@@ -0,0 +1,16 @@
+JIC Bio Image
+=============
+
+.. image:: https://badge.fury.io/py/jicbioimage.svg
+   :target: http://badge.fury.io/py/jicbioimage
+   :alt: PyPi package
+
+The ``jicbioimage`` Python package makes it easy to explore microscopy
+data in a programmatic fashion. Exploring images via coding means that the
+exploratory work becomes recorded and reproducible.  Furthermore, it makes it
+easier to convert the exploratory work into (semi) automated analysis work
+flows.
+
+- GitHub: https://github.com/JIC-CSB/jicbioimage
+- PyPI: https://pypi.python.org/pypi/jicbioimage
+- Free software: MIT License
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+readme = open('README.rst').read()
+
+version = "0.4.0"
+
+setup(name='jicbioimage',
+      packages=['jicbioimage', ],
+      version=version,
+      description='Python package designed to make it easy to work with bio images.',
+      long_description=readme,
+      author='Tjelvar Olsson',
+      author_email = 'tjelvar.olsson@jic.ac.uk',
+      url = 'https://github.com/JIC-CSB/jicbioimage',
+      download_url = 'https://github.com/JIC-CSB/jicbioimage/tarball/{}'.format(version),
+      license='MIT',
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+      ],
+      keywords = ['microscopy', 'image analysis'],
+)

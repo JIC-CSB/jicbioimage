247dc495fbce568978d6b3cb10ae922b3faa136e
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2016-06-30 13:54:31 +0100

    Updated version to 0.12.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ author = u'Tjelvar Olsson and Matthew Hartley'
 # built documents.
 #
 # The short X.Y version.
-version = '0.10.1'
+version = '0.12.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 readme = open('README.rst').read()
 
-version = "0.10.1"
+version = "0.12.0"
 
 setup(name='jicbioimage',
       packages=['jicbioimage', ],

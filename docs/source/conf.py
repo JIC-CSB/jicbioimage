ce4dc2fb32e34513cf19590ed5f24eea7ab8fddb
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-07-20 09:47:22 +0100

    Updated version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,7 +60,7 @@ author = u'Tjelvar Olsson and Matthew Hartley'
 # built documents.
 #
 # The short X.Y version.
-version = '0.13.0'
+version = '0.13.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
